f74d75ebe805d0db2bfd265b789420ca085624e9
Diego F.(i7-ubuntu) <diegoefe@gmail.com>
2023-07-08 17:04:03 -0300

    Fixed MysqlTypeWriter. Added SqliteTypeWriter

--- src/writers.rs
+++ src/writers.rs
@@ -22,10 +22,25 @@ impl TypeWriter for MysqlTypeWriter {
         match field_type {
             FieldType::Int => "int".to_owned(),
             FieldType::BigInt => "bigint".to_owned(),
-            FieldType::Txt => "varchar".to_owned(),
-            FieldType::Bool => "bool".to_owned(),
+            FieldType::Txt => "text".to_owned(),
+            FieldType::Bool => "bit".to_owned(),
             FieldType::Dbl => "double".to_owned(),
-            FieldType::AutoInc => "int autoincrement".to_owned(),
+            FieldType::AutoInc => "integer auto_increment".to_owned(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct SqliteTypeWriter {}
+impl TypeWriter for SqliteTypeWriter {
+    fn type_to_sql(&self, field_type:&FieldType) -> String {
+        match field_type {
+            FieldType::Int => "integer".to_owned(),
+            FieldType::BigInt => "integer".to_owned(),
+            FieldType::Txt => "text".to_owned(),
+            FieldType::Bool => "integer".to_owned(),
+            FieldType::Dbl => "real".to_owned(),
+            FieldType::AutoInc => "autoincrement".to_owned(),
         }
     }
 }
