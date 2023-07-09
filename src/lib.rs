07bb9afc6286f091ad1350906d739a3dd3bc31d3
Diego F.(i7-ubuntu) <diegoefe@gmail.com>
2023-07-09 11:34:05 -0300

    Lib documented

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
-pub mod util;
-pub mod sql;
+//! A library for DML generation from code or YAML files
+
+mod gen;
+pub use gen::Processor;
+
+/// TypeWriters
+///
+/// Defines TypeWriters for Postgresql (default), Mysql and Sqlite
 pub mod type_writers;
 
-mod generators;
-pub use generators::Generator;
+/// SQL module
+///
+/// Defines many type of database objects to generate SQL statements from
+pub mod sql;
+
+/// Util module
+///
+/// Contains easy to use functions to read and write to YAML files
+pub mod util;
