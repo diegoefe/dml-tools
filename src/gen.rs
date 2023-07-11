86311f0290f018b958f8562f1bc0c942835d4def
Diego F.(i7-ubuntu) <diegoefe@gmail.com>
2023-07-11 09:09:50 -0300

    Added Processor::new_with_objects()

--- src/gen.rs
+++ src/gen.rs
@@ -1,5 +1,7 @@
 use crate::sql::{DBObject, TypeWriter};
 use crate::type_writers::Postgresql;
+use crate::util::write_yaml_to_file;
+use std::error::Error;
 
 pub type BxTypeWriter = Box<dyn TypeWriter>;
 
@@ -25,6 +27,14 @@ impl <'a> Processor<'a> {
             type_writer,
         }
     }
+    /// Create a new Processor optionally specifying a TypeWriter to use
+    pub fn new_with_objects(objects:&'a Vec<Box<dyn DBObject>>, type_writer:Option<BxTypeWriter>) -> Self {
+        let mut me = Processor::new(type_writer);
+        for obj in objects.iter() {
+            me.objs.push(Box::new(obj.as_ref()))
+        }
+        me
+    }
     /// Add a DB object
     pub fn add(&mut self, object:&'a dyn DBObject) -> &Self {
         self.objs.push(Box::new(object));
@@ -45,5 +55,9 @@ impl <'a> Processor<'a> {
     /// Get a String with all of the SQL statments
     pub fn to_string(&self) -> String {
         self.sql_statements().join("\n")
+    }
+    /// Write objects to a YAML file
+    pub fn write_to_file(&self, file_name:&str) -> Result<(), Box<dyn Error>> {
+        Ok(write_yaml_to_file(file_name, &self.objs)?)
     }
 }
