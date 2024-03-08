bf91a2bc47149d04c7a9adc703c2bf3d4232620c
Diego Florio (I) <diegoefe@gmail.com>
2024-03-08 07:49:19 -0300

    Added cargo-first

--- scripts/run_all.cmd
+++ scripts/run_all.cmd
@@ -1,11 +1,14 @@
 @echo off
+@REM cargo install cargo-first
 
+@REM set cmd=cargo
+set cmd=cargo first
 echo "Running rust tests"
 set RUST_TEST_THREADS=1
-rem cargo test -- --nocapture --test-threads=1
-cargo test -- --nocapture
+rem %cmd% test -- --nocapture --test-threads=1
+%cmd% test -- --nocapture
 
 rem examples
-cargo run --example read-file-plus-code-dml-generation
-cargo run --example read-yaml
-cargo run --example generate-dmls
+%cmd% run --example read-file-plus-code-dml-generation
+%cmd% run --example read-yaml
+%cmd% run --example generate-dmls
--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,12 +1,15 @@
+# cargo install cargo-first
 #!/usr/bin/env bash
 set -e
 
+cmd="cargo first"
+# cmd=cargo
 echo "Running rust tests"
 export RUST_TEST_THREADS=1
-# cargo test -- --nocapture --test-threads=1
-cargo test -- --nocapture
+# ${cmd} test -- --nocapture --test-threads=1
+${cmd} test -- --nocapture
 
 # examples
-cargo run --example read-file-plus-code-dml-generation
-cargo run --example read-yaml
-cargo run --example generate-dmls
+${cmd} run --example read-file-plus-code-dml-generation
+${cmd} run --example read-yaml
+${cmd} run --example generate-dmls
